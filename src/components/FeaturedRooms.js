e286badfc1776e2f2bf5d1c53cd5f33bb5859a2c
Phan Hai Dang <drom97977@gmail.com>
2022-05-20 12:22:34 +0700

    Add: Room Component

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,14 +1,20 @@
 import React, { useContext } from "react";
 import { RoomContext } from "../context";
 import Loading from "./Loading";
-
+import Room from "./Room";
+import Title from "./Title";
 const FeaturedRooms = () => {
-    const { featuredRooms} = useContext(RoomContext);
-    console.log(featuredRooms);
+    const { featuredRooms, loading } = useContext(RoomContext);
+
+    const rooms = featuredRooms.map(room => {
+        return <Room key={room.id} room={room}/>
+    })
+
     return (
-        <div>
-            <Loading/>
-        </div>
+        <section className="featured-rooms">
+            <Title title="featured rooms" />
+            { loading ? <Loading /> :  rooms }
+        </section>
     )
 }
 
