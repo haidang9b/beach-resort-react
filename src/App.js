f55b764086ce312c15c80132a19baa509a7e07eb
Phan Hai Dang <drom97977@gmail.com>
2022-05-20 10:56:29 +0700

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
+import './App.css';
 
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-      <Home />
-      <Rooms />
-      <SingleRoom />
-      <Error />
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route path="/rooms" element={<Rooms/>}/>
+        <Route path="/rooms/:slug" element={<SingleRoom/>}/>
+        <Route element={<Error/>}/>
+      </Routes>
     </>
   );
 }
