453ad290b0526561cfd3f7dd8ab14caf1bf02780
Phan Hai Dang <drom97977@gmail.com>
2022-05-24 11:26:00 +0700

    Setup: Room Filter

--- src/context.js
+++ src/context.js
@@ -8,16 +8,31 @@ class RoomProvider extends Component {
         sortedRooms: [],
         featuredRooms: [],
         loading: true,
+        type: 'all',
+        capacity: 1,
+        price: 0,
+        minPrice: 0,
+        maxPrice: 0,
+        minSize: 0,
+        maxSize: 0,
+        breakfast: false,
+        pets: false
     }
     // getData
     componentDidMount() {
         let rooms = this.formatData(items)
         let featuredRooms = rooms.filter(room => room.featured === true);
+        let maxPrice = Math.max(...rooms.map(item => item.price));
+        let maxSize = Math.max(...rooms.map(item => item.size));
+
         this.setState({
             rooms,
             featuredRooms,
             sortedRooms: rooms,
-            loading: false
+            loading: false,
+            price: maxPrice,
+            maxPrice,
+            maxSize
         })
     }
 
@@ -35,6 +50,17 @@ class RoomProvider extends Component {
         let tempRooms = [...this.state.rooms];
         const room = tempRooms.find(room => room.slug === slug);
         return room;
+    }
+
+    handleChange = event => {
+        const type = event.target.type;
+        const name = event.target.name;
+        const value = event.target.value;
+        console.log(type, name, value);
+    }
+
+    filterRooms = () => {
+        console.log('filterRooms');
     }
 
     render() {
