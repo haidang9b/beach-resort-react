160ff025e02373735e1a67ca853cd78340c10942
Phan Hai Dang <drom97977@gmail.com>
2022-05-24 16:36:40 +0700

    Add: API Contentful

---
+++ src/Contentful.js
@@ -0,0 +1,6 @@
+import { createClient } from "contentful";
+
+export default createClient({
+    space: "s0hlk91pnrll",
+    accessToken: "Ce-MC-Gm2zNUMSm6oMfj1_T-z2uWP80pAxkgdbPLzfU"
+})
--- src/context.js
+++ src/context.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 import items from "./data";
+import Client from "./Contentful"
+
+Client.getEntries({
+    content_type: "beachResortRoom",
+}).then(response => { console.log(response.items) })
+
 const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
@@ -19,21 +25,32 @@ class RoomProvider extends Component {
         pets: false
     }
     // getData
-    componentDidMount() {
-        let rooms = this.formatData(items)
-        let featuredRooms = rooms.filter(room => room.featured === true);
-        let maxPrice = Math.max(...rooms.map(item => item.price));
-        let maxSize = Math.max(...rooms.map(item => item.size));
+    getData = async () => {
+        try {
+            let response = await Client.getEntries({
+                content_type: "beachResortRoom",
+                order: "sys.createdAt",
+            })
+            let rooms = this.formatData(response.items)
+            let featuredRooms = rooms.filter(room => room.featured === true);
+            let maxPrice = Math.max(...rooms.map(item => item.price));
+            let maxSize = Math.max(...rooms.map(item => item.size));
+            this.setState({
+                rooms,
+                featuredRooms,
+                sortedRooms: rooms,
+                loading: false,
+                price: maxPrice,
+                maxPrice,
+                maxSize
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-        this.setState({
-            rooms,
-            featuredRooms,
-            sortedRooms: rooms,
-            loading: false,
-            price: maxPrice,
-            maxPrice,
-            maxSize
-        })
+    componentDidMount() {
+        this.getData();
     }
 
     formatData(items) { 
