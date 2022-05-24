c2e99f7f35c00dacf0fdc0f8037b25de3c026988
Phan Hai Dang <drom97977@gmail.com>
2022-05-24 16:56:36 +0700

    Fix: enviroment key

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from "contentful";
 
 export default createClient({
-    space: "s0hlk91pnrll",
-    accessToken: "Ce-MC-Gm2zNUMSm6oMfj1_T-z2uWP80pAxkgdbPLzfU"
+    space: process.env.REACT_APP_SPACE_KEY,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN_KEY,
 })
