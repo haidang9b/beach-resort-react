258148da63ebde4251e359873f8a632b443d14d6
Phan Hai Dang <drom97977@gmail.com>
2022-05-24 11:20:29 +0700

    Add: RoomList Component

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -1,19 +1,18 @@
 import React from "react";
-import { Route, Routes, Redirect } from "react-router-dom";
 import RoomFilter from "./RoomFilter";
 import RoomList from "./RoomList";
 import { withRoomConsumer } from "../context";
 import Loading from "./Loading";
 
 const RoomsContainer = ({context}) => {
-    const { rooms, sortedRooms, featuredRooms, loading } = context;
+    const { rooms, sortedRooms, loading } = context;
     if (loading) {
         return <Loading />;
     }
     return (
         <div>
             <RoomFilter rooms={rooms}/>
-            <RoomList rooms={rooms}/>
+            <RoomList rooms={sortedRooms}/>
         </div>
     );
 }
